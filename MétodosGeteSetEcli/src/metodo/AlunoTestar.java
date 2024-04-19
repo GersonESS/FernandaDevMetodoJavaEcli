86f45221190d470466f4464d68144cb7607eb78a
Gerson Santos <gersones@outlook.com>
2024-04-19 18:18:04 -0300

    a

--- MétodosGeteSetEcli/src/metodo/AlunoTestar.java
+++ MétodosGeteSetEcli/src/metodo/AlunoTestar.java
@@ -3,7 +3,6 @@ package metodo;
 public class AlunoTestar {
 
 	public static void main(String[] args) {
-		public static void main(String[] args) {
 	        Aluno aluno1 = new Aluno();
 	        aluno1.setNome("Gerson");
 	        System.out.println(aluno1.getNome());
