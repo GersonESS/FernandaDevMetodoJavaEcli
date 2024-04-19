1c6c54f146c3994e3bd6361cc2ab85f897adcb2a
Gerson Santos <62805855+GersonESS@users.noreply.github.com>
2024-04-19 18:08:55 -0300

    a

---
+++ MétodosGeteSetEcli/src/metodo/Aluno.java
@@ -0,0 +1,23 @@
+package metodo;
+
+public class Aluno {
+	private String nome;
+    public int idade;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setIdade(int idade) {
+        if (idade > 0 && idade < 100) {
+            this.idade = idade;
+            System.out.println("Idade Cadastrada");
+        } else {
+            System.out.println("Idade Erro");
+        }
+    }
+}
---
+++ MétodosGeteSetEcli/src/metodo/AlunoTestar.java
@@ -0,0 +1,14 @@
+package metodo;
+
+public class AlunoTestar {
+
+	public static void main(String[] args) {
+		public static void main(String[] args) {
+	        Aluno aluno1 = new Aluno();
+	        aluno1.setNome("Gerson");
+	        System.out.println(aluno1.getNome());
+	        aluno1.setIdade(50);
+
+	}
+
+}
